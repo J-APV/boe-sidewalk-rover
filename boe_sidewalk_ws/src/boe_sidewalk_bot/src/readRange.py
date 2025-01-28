cb21dbb13977fcbf6b6b6f420fbd4338f960ad42
Jose <joseportillo0823@gmail.com>
2025-01-27 16:17:59 -0800

    Activate 3 ultrasonic sensors in readRange.py

--- boe_sidewalk_ws/src/boe_sidewalk_bot/src/readRange.py
+++ boe_sidewalk_ws/src/boe_sidewalk_bot/src/readRange.py
@@ -3,8 +3,10 @@
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Int16
+from std_msgs.msg import Float64
 from sensor_msgs.msg import Range
 from geometry_msgs.msg import Twist
+from collections import deque
 # import datetime as time
 
 
@@ -13,35 +15,48 @@ SIZE = 5
 class ReceiveRange:
     def __init__(self):
         self.msg_sub = []
+        self.measurements_queue = deque(maxlen=10)
         self.rate = rospy.Rate(1)
-        for x in range(SIZE):
-            self.msg_sub.append(rospy.Subscriber("/pi_sonar/sonar_" + str(x), Range, self.collect_data_callback))
+        #for x in range(1, 4):
+        self.msg_sub.append(rospy.Subscriber("/pi_sonar/sonar_" + str(3), Range, self.collect_data_callback, callback_args=str(3)))
         self.msg_pub = rospy.Publisher('is_safe', Int16, queue_size=1)
+        self.avg_pub = rospy.Publisher('avg_distance', Float64, queue_size=1)
+        self.speed_pub = rospy.Publisher('current_speed', Float64, queue_size=1)
         self.msg_speed_sub = rospy.Subscriber("/cmd_vel", Twist, self.collect_speed_callback)
         self.msg = 1
+        self.avg_measurement = 0.0
+        self.speed = 0.0
+        
         self.safe_distance = 0.5
 
-    def collect_data_callback(self, msg):
-        print("received - ", msg.range)
+    def collect_data_callback(self, msg, sonarNumber):
+        #print("Sonar " + sonarNumber)
+        #print(" received - ", msg.range)	
         self.measurements_queue.append(msg.range)
-        if len(self.measurements_queue) == 10:
-            avg_measurement = sum(self.measurements_queue) / 10
-            
-        if msg.range > self.safe_distance:
-            self.msg = 1
-        else:
-            self.msg = 0
-        self.rate.sleep()
+        print(len(self.measurements_queue))
+        if len(self.measurements_queue) == self.measurements_queue.maxlen:
+            self.avg_measurement = sum(self.measurements_queue) / self.measurements_queue.maxlen
+            print("avg measurement" + str(self.avg_measurement))
+            if self.avg_measurement > self.safe_distance:
+               self.msg = 1
+            else:
+               self.msg = 0
+        
+        
+        #self.rate.sleep()
 
     def collect_speed_callback(self, msg):
-        print("current speed of rover is ", msg.linear.x)
-        self.rate.sleep()
+        #print("current speed of rover is ", msg.linear.x)
+        self.speed = msg.linear.x
+        #self.rate.sleep()
 
     def send_msg(self):
         while not rospy.is_shutdown():
             self.msg_pub.publish(self.msg)
-            self.rate.sleep()
-
+            self.avg_pub.publish(self.avg_measurement)
+            self.speed_pub.publish(self.speed)
+            #self.rate.sleep()
+    
 
 if __name__ == '__main__':
     rospy.init_node('receive_range_data')
